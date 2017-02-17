9339d783b764240441e291c159d0c0a4ec3deacf
Moreno B <moreno_bralts@hotmail.com>
2017-02-17 15:36:54 +0100

    Removed Singleton class inheritance

---
+++ DrawHandler.cs
@@ -0,0 +1,34 @@
+﻿namespace GraphicalEditor
+{
+    public sealed class DrawHandler
+    {
+        private Form form;
+
+        private static readonly DrawHandler instance = new DrawHandler();
+
+        // Explicit static constructor to tell C# compiler
+        // not to mark type as beforefieldinit
+        static DrawHandler()
+        {
+            
+        }
+
+        private DrawHandler()
+        {
+        }
+
+        public static DrawHandler Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        public void Initialize(Form form)
+        {
+            this.form = form;
+        }
+
+    }
+}
