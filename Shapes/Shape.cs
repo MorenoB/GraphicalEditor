3075c619507aae34d30c1087f20a92855b8838c5
Moreno B <moreno_bralts@hotmail.com>
2017-02-17 15:37:33 +0100

    Added Shapetype enum in Shape class & Setters will now modify the 'dirty' flag

--- Shapes/Shape.cs
+++ Shapes/Shape.cs
@@ -2,12 +2,56 @@
 {
     class Shape
     {
-        public int Location_X { get { return location_x; } }
-        public int Location_Y { get { return location_y; } }
+        public int Location_X
+        {
+            get { return location_x; }
+            set
+            {
+                if (location_x == value) return;
 
-        public int Width { get { return width; } }
-        public int Length { get { return length; } }
+                location_x = value;
+                dirty = true;
+            }
+        }
+        public int Location_Y
+        {
+            get { return location_y; }
+            set
+            {
+                if (value == location_y) return;
 
+                location_y = value;
+                dirty = true;
+            }
+        }
+
+        public int Width
+        {
+            get { return width; }
+            set
+            {
+                if (value == width) return;
+
+                width = value;
+                dirty = true;
+            }
+        }
+        public int Length
+        {
+            get { return length; }
+            set
+            {
+                if (value == length) return;
+
+                length = value;
+                dirty = true;
+            }
+        }
+        public ShapeTypeEnum ShapeType { get { return shapeType; } }
+
+        public enum ShapeTypeEnum { RECTANGLE, CIRCLE };
+
+        private ShapeTypeEnum shapeType;
         public bool IsDirty { get { return dirty; } set { dirty = value; } }
 
         private bool dirty;
@@ -18,12 +62,14 @@
         private int width;
         private int length;
 
-        public Shape(int location_x, int location_y, int width, int length)
+        public Shape(ShapeTypeEnum shapeType, int location_x, int location_y, int width, int length)
         {
             this.location_x = location_x;
             this.location_y = location_y;
             this.width = width;
             this.length = length;
+            this.shapeType = shapeType;
+            this.dirty = true;
         }
     }
 }
